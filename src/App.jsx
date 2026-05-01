f54f68c6638a7a54ee6f6dd80700aac131b97a70
ATPaing <aungthetpaing.2001@gmail.com>
2026-05-01 22:00:27 +0100

    set up dashboard.jsx and update some css

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 // pages
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 // css
 import "./App.css";
@@ -15,7 +16,8 @@ function App() {
             <Routes>
                 <Route path="/" element={<Signup />} />
                 <Route path="/signup" element= {<Signup />} />
-                <Route path="/login" element = {<Login/>} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </>
     );
